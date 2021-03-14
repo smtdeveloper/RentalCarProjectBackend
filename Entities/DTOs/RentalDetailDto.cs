8ed52e12b3759698e4d9f23c1a9b15ff3eaeea54
Samet Akca <+akcasamet99@users.noreply.github.com>
2021-03-14 17:26:16 +0300

    Rental için DTO Yapıldı :)

--- Entities/DTOs/RentalDetailDto.cs
+++ Entities/DTOs/RentalDetailDto.cs
@@ -8,7 +8,7 @@ namespace Entities.DTOs
     public class RentalDetailDto : IDto
     {
         public int RentalId { get; set; }
-        public string BrandName { get; set; }
+        public string BrandName { get; set; } 
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime RentDate { get; set; }
