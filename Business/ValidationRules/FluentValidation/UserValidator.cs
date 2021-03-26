76cea29f4aa154e61c3dad48abf5e03a5066f15f
Samet Akca <+akcasamet99@users.noreply.github.com>
2021-03-26 23:23:42 +0300

    55

--- Business/ValidationRules/FluentValidation/UserValidator.cs
+++ Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -11,8 +11,6 @@ namespace Business.ValidationRules.FluentValidation
     {
         public UserValidator()
         {
-      
-
             RuleFor(u => u.FirstName).NotEmpty();
             RuleFor(u => u.FirstName).MinimumLength(2);
             RuleFor(u => u.LastName).NotEmpty();
