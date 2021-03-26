cc4f9812bd4f48535d1e6ee006622a6df4827b9c
Samet Akca <+akcasamet99@users.noreply.github.com>
2021-03-26 23:20:46 +0300

    JWT  KURDUK :9

--- Business/ValidationRules/FluentValidation/UserValidator.cs
+++ Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Business.ValidationRules.FluentValidation
-{
+{ 
     public class UserValidator : AbstractValidator<User>
     {
         public UserValidator()
