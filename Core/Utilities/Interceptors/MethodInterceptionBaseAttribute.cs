b50866a06ac53777ae687c0f6c4933e416962f98
Samet Akca <+akcasamet99@users.noreply.github.com>
2021-03-28 12:26:33 +0300

    Cache, Transaction ve Performance aspectlerini yazdım :)

--- Core/Utilities/Interceptors/MethodInterceptionBaseAttribute.cs
+++ Core/Utilities/Interceptors/MethodInterceptionBaseAttribute.cs
@@ -7,7 +7,7 @@ namespace Core.Utilities.Interceptors
 {
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]  
     public class MethodInterceptionBaseAttribute : Attribute, IInterceptor
-    {
+    { 
         public int Priority { get; set; }
             
         public virtual void Intercept(IInvocation invocation)
