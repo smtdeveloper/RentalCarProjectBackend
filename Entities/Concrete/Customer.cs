7e1dec0e8cae39bdb2ca1c8a665b618cd2e9f7f6
Samet Akca <+akcasamet99@users.noreply.github.com>
2021-04-06 02:30:42 +0300

    19 ödevv

--- Entities/Concrete/Customer.cs
+++ Entities/Concrete/Customer.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Entities.Concrete
 {
-    public class Customer : IEntity
+    public class Customer : IEntity  
     {
         public int CustomerId { get; set; }
         public int UserId { get; set; }
